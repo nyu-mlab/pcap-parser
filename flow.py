6fff1f84b02ea5006a66d337017f96bb989fe047
Rameen Mahmood <rkm352@nyu.edu>
2025-07-01 21:46:00 -0400

    include mDNS and UPnP (SSDP) in flow aggregation

--- flow.py
+++ flow.py
@@ -14,50 +14,68 @@ Note:
     The script is platform-independent but requires Python and pandas to be installed. Make sure file paths and permissions are correctly configured according to your operating system's requirements.
 
 """
-
 import pandas as pd
 import sys
 import tldextract
 
 def extract_main_domain(hostname):
     try:
-        extracted = tldextract.extract(hostname) # Extracts registered domain: main domain and TLD
+        extracted = tldextract.extract(hostname)
         return f"{extracted.domain}.{extracted.suffix}"
     except:
         return None
 
 def process_pcap_data(input_csv, output_csv):
     df = pd.read_csv(input_csv)
-    df['frame.time_epoch'] = pd.to_datetime(df['frame.time_epoch'], unit='s') # Convert to timestamps
+    df['frame.time_epoch'] = pd.to_datetime(df['frame.time_epoch'], unit='s')
 
-    df = df[df['_ws.col.Protocol'].isin(['TCP', 'TLSv1.2', 'UDP'])] # Filtering out non-IP/TCP/UDP protocols for accurate flow information
+    df = df[df['_ws.col.Protocol'].isin(['TCP', 'TLSv1.2', 'UDP'])]
 
-    # Sort by grouping columns and timestamp for accurate inter-arrival times
-    df = df.sort_values(by=['ip.src', 'ip.dst', 'tcp.srcport', 'tcp.dstport', 'udp.srcport', 'udp.dstport', '_ws.col.Protocol', 'frame.time_epoch'])
-    grouped = df.groupby(['ip.src', 'ip.dst', 'tcp.srcport', 'tcp.dstport', 'udp.srcport', 'udp.dstport', '_ws.col.Protocol'])
-    # Calculating inter-arrival times within each flow
-    df['inter_arrival_time'] = df.groupby(['ip.src', 'ip.dst', 'tcp.srcport', 'tcp.dstport', 'udp.srcport', 'udp.dstport', '_ws.col.Protocol'])['frame.time_epoch'].diff().dt.total_seconds()
+    df = df.sort_values(by=[
+        'ip.src', 'ip.dst',
+        'tcp.srcport', 'tcp.dstport',
+        'udp.srcport', 'udp.dstport',
+        '_ws.col.Protocol', 'frame.time_epoch'
+    ])
+    df['inter_arrival_time'] = df.groupby([
+        'ip.src', 'ip.dst',
+        'tcp.srcport', 'tcp.dstport',
+        'udp.srcport', 'udp.dstport',
+        '_ws.col.Protocol'
+    ])['frame.time_epoch'].diff().dt.total_seconds()
 
-    # Apply domain extraction to hostname columns
     df['src_main_domain'] = df['src_hostname'].apply(extract_main_domain)
     df['dst_main_domain'] = df['dst_hostname'].apply(extract_main_domain)
 
-    # Aggregate data for each flow
+    grouped = df.groupby([
+        'ip.src', 'ip.dst',
+        'tcp.srcport', 'tcp.dstport',
+        'udp.srcport', 'udp.dstport',
+        '_ws.col.Protocol'
+    ])
+
     flows = grouped.agg(
         start_ts=('frame.time_epoch', 'min'),
         end_ts=('frame.time_epoch', 'max'),
         byte_count=('frame.len', 'sum'),
         packet_count=('frame.len', 'size'),
-        avg_inter_arrival_time=('inter_arrival_time', 'mean'),  # Calculate average inter-arrival time
+        avg_inter_arrival_time=('inter_arrival_time', 'mean'),
         src_hostname=('src_hostname', 'first'),
         dst_hostname=('dst_hostname', 'first'),
         src_main_domain=('src_main_domain', 'first'),
-        dst_main_domain=('dst_main_domain', 'first')
-    )
-    flows.reset_index(inplace=True)
+        dst_main_domain=('dst_main_domain', 'first'),
+        user_agent_info=('http.user_agent', 'first')
+    ).reset_index()
 
-    # Specify order
-    columns_order = ['start_ts', 'end_ts', 'ip.src', 'ip.dst', 'tcp.srcport', 'tcp.dstport', 'udp.srcport', 'udp.dstport', '_ws.col.Protocol', 'byte_count', 'packet_count', 'avg_inter_arrival_time', 'src_hostname', 'dst_hostname', 'src_main_domain', 'dst_main_domain']
+    columns_order = [
+        'start_ts', 'end_ts', 'ip.src', 'ip.dst',
+        'tcp.srcport', 'tcp.dstport', 'udp.srcport', 'udp.dstport',
+        '_ws.col.Protocol', 'byte_count', 'packet_count', 'avg_inter_arrival_time',
+        'src_hostname', 'dst_hostname',
+        'src_main_domain', 'dst_main_domain',
+        'user_agent_info'
+    ]
+
     flows = flows[columns_order]
     flows.to_csv(output_csv, index=False)
 
